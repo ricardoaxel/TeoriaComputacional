98c0ce6de08fe0e197f9c41e352f2546dfbf16ea
ricardoaxel <elchetumal@gmail.com>
2018-05-28 23:10:23 -0500

    Practica 10 concluida

--- 10.py
+++ 10.py
@@ -1,32 +1,35 @@
 #VERIFICADOR DE PALINDROMOS
-print("Verificador de palindromos")
+print("Verificador de palindromos, Ingrese la cadena a evaluar:")
 
-cad = input() #cadena a evaluar
+ingr = input() #cadena a evaluar
+cad= list(ingr)
 
 tamcad = len(cad)
+verificador = 0
 micha = int(tamcad/2)
 
-verificador = 0
-aux = 0
+if(tamcad%2 == 1): #si la cadena es impar tomamos la letra del centro como mitad
+	micha = int(tamcad/2)
 
-for i in range(0,micha):
-	print("Comparando "+cad[i]+" con "+cad[-i-1])
-	if cad[i] == cad[-i-1]:
+if(tamcad%2 == 0): #si no es par creamos un centro 'x'
+	cad.insert(micha,"x")
+	
+print(cad)
+
+print("Comenzando en: "+cad[micha]+"\n")
+for i in range(1,micha+1):
+	print("Comparando "+cad[micha-i]+" con "+cad[micha+i])
+	if cad[micha-i] == cad[micha+i]:
 		verificador = verificador+1
 		print("Letras iguales")
 	else:
-		print("letras distintas")
+		print("Letras distintas")
 		break
-
 
 if(verificador==micha):
 	print("\nEs un palíndromo")
 
 else:
-	print("\nNo es un palíndromo")
-
-
-
-
+	print("\nNO es un palíndromo")
 
 
--- prueba.py
+++ prueba.py
@@ -1,50 +1,35 @@
-import random
+#VERIFICADOR DE PALINDROMOS
+print("Verificador de palindromos, Ingrese la cadena a evaluar:")
 
-# Abre archivo para escribir
-archivo = open('datos1.txt','w')
+ingr = input() #cadena a evaluar
+cad= list(ingr)
 
-print ("Combinaciones de 1 y 0. \n")
-opc = input("Que desea hacer\n 1)Ingresar una potencia \n 2)Generar potencia \n")
+tamcad = len(cad)
+verificador = 0
+micha = int(tamcad/2)
 
-if (opc =="1"):
-	potencia = int(input("Ingrese una potencia:\n "))
-else: 
-	potencia = random.randrange(100)
+if(tamcad%2 == 1): #si la cadena es impar tomamos la letra del centro como mitad
+	micha = int(tamcad/2)
 
-
-
-print ("Potencia elegida:", potencia, "\nSe generara un archivo con:  combinaciones...")
-
-archivo.write("{")
-
-
-for x in range(0,potencia): 
-
-	numcom = 2 ** potencia #numero de combinaciones
-	auxpot = potencia
-
-	while numcom > 0:
-
-		if numcom != 2**potencia:
-
-			archivo.write(str(bin(numcom)[2:potencia+2]).zfill(potencia))
-			archivo.write (",")
-		
-		numcom = numcom - 1
-
-		if numcom == 0: #para imprimir la combinación final (o*p)
-			while auxpot > 0:
-				archivo.write("0")
-				auxpot = auxpot - 1
-
-	potencia = potencia - 1 #para ir bajando de potencia
-
-	if (potencia != 0): #Validacion para que no aparezca "," al final
-		archivo.write(",")
-
+if(tamcad%2 == 0): #si no es par creamos un centro 'x'
+	cad.insert(micha,"x")
 	
+print(cad)
 
-	
-archivo.write("}")
-archivo.close
-print ("Listo")
+print("Comenzando en: "+cad[micha]+"\n")
+for i in range(1,micha+1):
+	print("Comparando "+cad[micha-i]+" con "+cad[micha+i])
+	if cad[micha-i] == cad[micha+i]:
+		verificador = verificador+1
+		print("Letras iguales")
+	else:
+		print("Letras distintas")
+		break
+
+if(verificador==micha):
+	print("\nEs un palíndromo")
+
+else:
+	print("\nNO es un palíndromo")
+
+
